cc33aa0d4a35abc10911a480e7b245e35c1304ce
2j2h5 <chawun36@gmail.com>
2024-11-30 18:22:09 +0900

    Enhance accuracy of classfier

--- train_hangul_conv_net.py
+++ train_hangul_conv_net.py
@@ -8,11 +8,11 @@ from common.trainer import Trainer
 
 (x_train, t_train), (x_test, t_test) = load_hangul(flatten=False)
 
-max_epochs = 30
+max_epochs = 20
 
 network = HangulConvNet(input_dim=(1,28,28), 
                         conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
-                        hidden_size=100, output_size=54, weight_init_std=0.005)
+                        hidden_size=500, output_size=54, weight_init_std=0.005)
                         
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
                   epochs=max_epochs, mini_batch_size=32,
